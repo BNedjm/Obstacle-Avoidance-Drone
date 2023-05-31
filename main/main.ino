0848762100f9ea2180929eda6698240c56018596
BNedjm <boukhedenna.nedjm@gmail.com>
2023-05-31 20:58:32 +0200

    analog write 10bits feedback testing

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,18 @@
 
 //We create variables for the time width values of each PWM input signal
 unsigned long counter_2, counter_3, current_count;
+unsigned long counter_6, counter_5;
 
 //We create 2 variables to stopre the previous value of the input signal (if LOW or HIGH)
 byte last_CH2_state, last_CH3_state;
+byte last_CH6_state, last_CH5_state;
 
 //To store the 1000us to 2000us value we create variables and store each channel
 int input_PITCH;    //In my case channel 2 of the receiver and pin D8 of arduino
 int input_THROTTLE; //In my case channel 3 of the receiver and pin D12 of arduino
+// Feedback variables
+int feedback_PITCH;
+int feedback_THROTTLE;
 
 // Define the PWM output pins
 const int PWM_PITCH_PIN = 9;    // Pin for PITCH output
@@ -32,8 +37,11 @@ void setup() {
   pinMode(echoPin, INPUT);
   
   PCICR |= (1 << PCIE0);    //enable PCMSK0 scan                                                 
-  PCMSK0 |= (1 << PCINT0);  //Set pin D8 trigger an interrupt on state change.                                               
-  PCMSK0 |= (1 << PCINT4);  //Set pin D12 trigger an interrupt on state change. 
+  PCMSK0 |= (1 << PCINT0);  // Set pin D8 trigger an interrupt on state change.
+  PCMSK0 |= (1 << PCINT4);  // Set pin D12 trigger an interrupt on state change.
+  PCMSK0 |= (1 << PCINT5);  // Set pin D5 trigger an interrupt on state change.
+  PCMSK0 |= (1 << PCINT6);  // Set pin D6 trigger an interrupt on state change.
+
    
   Serial.begin(9600);  
   while (!Serial); 
@@ -44,15 +52,17 @@ void loop() {
 
   input_PITCH;
   input_THROTTLE;
+  feedback_PITCH;
+  feedback_THROTTLE;
+
 
   // Map the input values to fit within the Serial Plotter range
   int value_PITCH = map(input_PITCH, 0, 2000, 0, resolution - 1);
   int value_THROTTLE = map(input_THROTTLE, 0, 2000, 0, resolution - 1);
+  // Map the feedback input values to fit within the Serial Plotter range
+  int feedback_value_PITCH = map(feedback_PITCH, 0, 2000, 0, resolution - 1);
+  int feedback_value_THROTTLE = map(feedback_THROTTLE, 0, 2000, 0, resolution - 1);
 
-
-  // Serial.print(input_PITCH);
-  // Serial.print(",");
-  // Serial.println(input_THROTTLE);
   
   // write the duty cycle
   analogWrite10Bit(PWM_PITCH_PIN, value_PITCH);
@@ -65,9 +75,23 @@ void loop() {
     long distance = getDistance(trigPin, echoPin);
 
     // Print the distance to the Serial Monitor
-    Serial.print("Distance: ");
-    Serial.print(distance);
-    Serial.println(" cm");
+    // Serial.print("Distance: ");
+    // Serial.print(distance);
+    // Serial.print(" cm || ");
+
+    // More Serial coms
+    Serial.print("THROTTLE: ");
+    Serial.print(value_THROTTLE);
+    Serial.print(" IN-->OUT ");
+    Serial.print(feedback_THROTTLE);
+    Serial.print(" MAPPED TO ");
+    Serial.println(feedback_value_THROTTLE);
+    // Serial.print(" || ");
+
+    // Serial.print("PITCH: ");
+    // Serial.print(input_PITCH);
+    // Serial.print(" IN-->OUT ");
+    // Serial.println(feedback_PITCH);
 
     // Update the previous time to the current time
     previousTime = currentTime;
@@ -134,4 +158,30 @@ ISR(PCINT0_vect){
     input_THROTTLE = current_count - counter_3;                            
 
   }
+
+  // Feedback input
+  ///////////////////////////////////////Channel 6
+  if(PINB & B01000000){  // pin D6 - B01000000
+    if(last_CH6_state == 0){
+      last_CH6_state = 1;
+      counter_6 = current_count;
+    }
+  }
+  else if(last_CH6_state == 1){
+    last_CH6_state = 0;
+    feedback_THROTTLE = current_count - counter_6;
+  }
+
+  ///////////////////////////////////////Channel 5
+  if(PINB & B00100000){  // pin D5 - B00100000
+    if(last_CH5_state == 0){
+      last_CH5_state = 1;
+      counter_5 = current_count;
+    }
+  }
+  else if(last_CH5_state == 1){
+    last_CH5_state = 0;
+    feedback_PITCH = current_count - counter_5;
+  }
+
 }
